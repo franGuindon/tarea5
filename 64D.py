a816f3dc3c53cc6b440b99c13adac99ccd66e8dc
franGuindon <fbadilla10@gmail.com>
2020-11-30 13:55:22 -0600

    creo que termine 64d

--- 64D.py
+++ 64D.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Nov 29 15:10:15 2020
-
-@author: Francis
+@author: Francis, Alejandro
 """
 
-"NEEDED FOR EVERYTHING"
 print(__doc__)
 
 from time import time
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 from sklearn import metrics
 from sklearn.cluster import KMeans
@@ -18,29 +16,17 @@ from sklearn.datasets import load_digits
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
-np.random.seed(42)
-
-scaler = StandardScaler()
-X_digits, y_digits = load_digits(return_X_y=True)
-data = scaler.fit_transform(X_digits)
-
-n_samples, n_features = data.shape
-n_digits = len(np.unique(y_digits))
-labels = y_digits
-
-sample_size = 300
-
 "Gotta make a printing function"
 def write_down(estimator="",name="",data=""):
-    col_labels = ["Initializer", "}",
-                  "Time (s)", ".3f}",
-                  "Inertia", ".0f}",
-                  "Homogeneity", ".3f}",
-                  "Completeness", ".3f}",
-                  "V-Measure", ".3f}",
-                  "ARI", ".3f}",
-                  "AMI", ".3f}",
-                  "Silhouette", ".3f}"]
+    col_labels = ["Initializer", "",
+                  "Time (s)", ".3f",
+                  "Inertia", ".0f",
+                  "Homogeneity", ".3f",
+                  "Completeness", ".3f",
+                  "V-Measure", ".3f",
+                  "ARI", ".3f",
+                  "AMI", ".3f",
+                  "Silhouette", ".3f"]
     cell_width = 13
     columns = len(col_labels)//2
     def hline():
@@ -67,7 +53,7 @@ def write_down(estimator="",name="",data=""):
     else:
         t0 = time()
         estimator.fit(data)
-        print("".join("{:<"+str(cell_width)+form\
+        print("".join("{:<"+str(cell_width)+form+"}"\
                       for i,form in enumerate(col_labels) if i%2==1)\
                .format(name,
                        time()-t0,
@@ -83,86 +69,147 @@ def write_down(estimator="",name="",data=""):
                        )
               )
 
-# Introduction
-write_down(name = "header")
+#%%
+"Global Variables"
+np.random.seed(42)
+
+scaler = StandardScaler()
+X_digits, y_digits = load_digits(return_X_y=True)
+data = scaler.fit_transform(X_digits)
+
+n_samples, n_features = data.shape
+n_digits = len(np.unique(y_digits))
+labels = y_digits # Used in metrics
+
+sample_size = 300 # Used for silhouette metric
+wholedatapca = PCA(n_components = 2)
+centroidpca = PCA(n_components = 2)
+reduced_data = wholedatapca.fit_transform(data)
+
+# Grid points
+h = .02
+x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
+y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+grid_points = np.c_[xx.ravel(), yy.ravel()]
+# high_grid_points = wholedatapca.inverse_transform(grid_points)
+#%%
+"Main Loop"
+write_down(name = "header") # Introduction
 for k in [3,10,20]:
-    # Save k-means++ model
+    
+    "Save k-means++ model"
     model = KMeans(init="k-means++",n_clusters=k,n_init=10)
-    # Log metrics
-    write_down(name = "_For {} Clusters".format(k))
-    write_down(model,"k-means++",data)
-    write_down(KMeans(init="random",n_clusters=k,n_init=10),"random",data)
+    
+    "Log metrics"
+    write_down(name = "_For {} Clusters".format(k)) # Section title
+    write_down(model,"k-means++",data) # Log k-means ++ model
+    write_down(KMeans(init="random",n_clusters=k,n_init=10),"random",data) # Log random model
     write_down(KMeans(init=PCA(n_components=k).fit(data).components_, n_clusters=k, n_init=1),
-               "PCA-based",data)
-    write_down(name = "footer")
-    # Centroid images
-    centroids = scaler.inverse_transform(model.cluster_centers_)
-    # centroid = centroids[0].reshape(8,8)
-    # plt.figure(k)
-    # plt.clf()
-    # plt.imshow(centroid,
-    #            interpolation="nearest",
-    #            cmap="gray")
-    # plt.show
-    num_row = 2**int(k/10)
-    num_col = 5 if k != 3 else 3
-    fig, axes = plt.subplots(num_row, num_col, figsize = (2*num_col,2.5*num_row))
+               "PCA-based",data) # Log PCA based model
+    write_down(name = "footer") # Section separator
+    
+    "Centroid images (special thanks to Mr Data Science at Medium.com)"
+    centroids = model.cluster_centers_
+    denormed_centroids = scaler.inverse_transform(centroids)
+    num_row = 2**int(k/10) # 1 if k=3, 2 if k=10, 4 if k=20
+    num_col = 5 if k != 3 else 3 # 3 if k=3, 5 otherwise
+    fig, axes = plt.subplots(num_row, num_col, figsize = (2*num_col,2.5*num_row)) # Subplot grid
     for i in range(k):
-        ax = axes[i//num_col, i%num_col] if k != 3 else axes[i]
-        ax.imshow(centroids[i].reshape(8,8), cmap="gray")
+        ax = axes[i//num_col, i%num_col] if k != 3 else axes[i] # Subplot coordinates
+        ax.imshow(denormed_centroids[i].reshape(8,8), cmap="gray")
         ax.set_title("Centroid #{}".format(i+1))
     plt.tight_layout()
     plt.show()
+    
+    "Scatter and partition plots (data PCA)"
+    reduced_model = KMeans(init="k-means++",n_clusters=k,n_init=10)
+    reduced_model.fit(reduced_data)
+    reduced_centroids = reduced_model.cluster_centers_
+    points_pred = reduced_model.predict(grid_points).reshape(xx.shape)
+    #points_pred = reduced_model.predict(high_grid_points).reshape(xx.shape)
+    plt.figure(k)
+    plt.clf()
+    plt.imshow(points_pred, interpolation="nearest",
+               extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+               cmap=plt.cm.tab20,
+               aspect='auto', origin='lower')
+    # 2D Data Scatter
+    batch_size = 500
+    print_data = np.array(random.sample(reduced_data.tolist(),batch_size))
+    plt.plot(print_data[:, 0], print_data[:, 1], 'k.', markersize=2)
+    # 2D Centroid Scatter
+    plt.scatter(reduced_centroids[:, 0], reduced_centroids[:, 1],
+            marker='p', s=70, linewidths=2,
+            color='w', zorder=10)
+    # 64D Centroid Scatter
+    whole_centroids = wholedatapca.transform(centroids)
+    plt.scatter(whole_centroids[:, 0], whole_centroids[:, 1],
+            marker='*', s=70, linewidths=2,
+            color='b', zorder=10)
+    # Figure formatting
+    plt.title("DIGIT CLUSTERING\n{} clusters, whole data PCA-reduction".format(k))
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+    plt.xticks(())
+    plt.yticks(())
+    plt.show()
+    
+    # IOU: This code block is quite similar to previous one and in the future
+    #      should be modulated
+    "Scatter and partition plots (centroid PCA)"
+    # fit PCA with model centroids, then transform centroids and data
+    centroid_centroids = centroidpca.fit_transform(centroids)
+    centroid_reduced_data = centroidpca.transform(data)
+    # train new reduced model with centroid transformed data
+    centroid_reduced_model = KMeans(init="k-means++",n_clusters=k,n_init=10)
+    centroid_reduced_model.fit(centroid_reduced_data)
+    centroid_reduced_centroids = centroid_reduced_model.cluster_centers_
+    points_pred = centroid_reduced_model.predict(grid_points).reshape(xx.shape)
+    #points_pred = centroid_model.predict(high_grid_points).reshape(xx.shape)
+    plt.figure(k)
+    plt.clf()
+    plt.imshow(points_pred, interpolation="nearest",
+               extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+               cmap=plt.cm.tab20,
+               aspect='auto', origin='lower')
+    # 2D Data Scatter
+    batch_size = 500
+    print_data = np.array(random.sample(centroid_reduced_data.tolist(),batch_size))
+    plt.plot(print_data[:, 0], print_data[:, 1], 'k.', markersize=2)
+    # 2D Centroid Scatter
+    plt.scatter(centroid_reduced_centroids[:, 0], centroid_reduced_centroids[:, 1],
+            marker='p', s=70, linewidths=2,
+            color='w', zorder=10)
+    # 64D Centroid Scatter
+    plt.scatter(centroid_centroids[:, 0], centroid_centroids[:, 1],
+            marker='*', s=70, linewidths=2,
+            color='b', zorder=10)
+    # Figure formatting
+    plt.title("DIGIT CLUSTERING\n{} clusters, centroid PCA-reduction".format(k))
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+    plt.xticks(())
+    plt.yticks(())
+    plt.show()
+    
+#%%
+# Debug block
 
-# # #############################################################################
-# # Visualize the results on PCA-reduced data
+# model = KMeans(init="k-means++",n_clusters=k,n_init=10)
+# model.fit(data)
+# datum = [1,2]
+# pca_obj = PCA(n_components = 2)
+# new_datum = pca_obj.fit(data).inverse_transform(datum).reshape(1,-1)
+# print(new_datum)
+# print(model.predict(new_datum))
 
-# reduced_data = PCA(n_components=2).fit_transform(data)
-# kmeans = KMeans(init='k-means++', n_clusters=n_digits, n_init=10)
-# kmeans.fit(reduced_data)
+# A = np.arange(1,5,1)
+# B = np.arange(2,4,1)
+# X,Y = np.meshgrid(A,B)
+# #print(np.c_[X.ravel(),Y.ravel()])
 
-# # Step size of the mesh. Decrease to increase the quality of the VQ.
-# h = .02     # point in the mesh [x_min, x_max]x[y_min, y_max].
-
-# # Plot the decision boundary. For that, we will assign a color to each
-# x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
-# y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
-# xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
-
-# # Obtain labels for each point in mesh. Use last trained model.
-# Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
-
-# # Put the result into a color plot
-# Z = Z.reshape(xx.shape)
-# plt.figure(1)
-# plt.clf()
-# plt.imshow(Z, interpolation='nearest',
-#            extent=(xx.min(), xx.max(), yy.min(), yy.max()),
-#            cmap=plt.cm.Paired,
-#            aspect='auto', origin='lower')
-
-# plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
-# # Plot the centroids as a white X
-# centroids = kmeans.cluster_centers_
-# plt.scatter(centroids[:, 0], centroids[:, 1],
-#             marker='x', s=169, linewidths=3,
-#             color='w', zorder=10)
-# plt.title('K-means clustering on the digits dataset (PCA-reduced data)\n'
-#           'Centroids are marked with white cross')
-# plt.xlim(x_min, x_max)
-# plt.ylim(y_min, y_max)
-# plt.xticks(())
-# plt.yticks(())
-# plt.show()
-
-# # #############################################################################
-# #
-
-# est = KMeans(init='k-means++', n_clusters=n_digits, n_init=10)
-# est.fit(data)
-# centroids = est.cluster_centers_
-# img = data[0].reshape(8,8)
-# img2 = centroids[3].reshape(8,8)
-# fig = plt.figure
-# plt.imshow(img2,cmap="gray")
-# plt.show
+# grid = [[1,2],
+#         [2,3],
+#         [3,4]]
+# print(random.sample(grid,2))
---
+++ 784D.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 30 13:54:16 2020
+
+@author: Francis
+"""
+
